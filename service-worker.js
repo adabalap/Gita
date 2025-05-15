1407dac29275caf8f6c05e0c4467d7a874f7684b
Phani Adabala <38834236+adabalap@users.noreply.github.com>
2025-05-16 00:26:54 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,36 @@
-// Service Worker for Bhagavad Gita PWA
-
 const CACHE_NAME = 'bhagavath-geetha-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
     '/manifest.json',
-    // Add paths to your icon files
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-    // Note: Tailwind CSS and Google Fonts are external and won't be cached by this simple service worker
+    '/images/splash-image.jpeg'
 ];
 
-// Install event: Cache core app shell files
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
     );
 });
 
-// Fetch event: Serve cached assets when offline
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                // No cache hit - fetch from network
-                return fetch(event.request);
-            })
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
 });
 
-// Activate event: Clean up old caches
 self.addEventListener('activate', (event) => {
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        // Delete old caches
+                    if (!cacheWhitelist.includes(cacheName)) {
                         return caches.delete(cacheName);
                     }
                 })
